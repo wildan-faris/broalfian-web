fe05f7707187a27bbd603f089ae56c099f8d9b2e
Alfian Ramadani <Alfianr792@gmail.com>
2024-04-24 09:30:41 +0800

    feat : Adding Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React from 'react';
+import HeroImg from '../assets/hero-img.jpg'
 
-export default function Hero() {
-  const [one, setPlusOne] = useState(1);
-
-  function handleClick() {
-    setPlusOne(one + 1);
-  }
-
+const Hero = () => {
   return (
-    <>
-      <p>{one}</p>
-      <button onClick={handleClick}>Click Me</button>
-    </>
+    <div className=" flex flex-col flex-grow space-y-7 items-center justify-center text-center">
+        <img src={HeroImg} className=" w-52 h-52 bg-white rounded-full" alt="Hero-profile-img" />
+        <div className=" w-6/12 ">
+          <h1 className=" text-5xl font-bold"><span className="text-transparent bg-clip-text bg-gradient-to-r from-[#B16CEA] to-[#FF7B5D]">Hello, I'm Alfian,</span> fullstack developer based in Indonesia</h1>
+        </div>
+        <div className="w-4/12">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque incidunt, earum ut ratione iure minus!</p>
+        </div>
+        <div className="flex-row space-x-10">
+          <button className="px-8 py-4 rounded-full bg-white text-black font-medium">GET IN TOUCH</button>
+          <button  className="px-8 py-4 border-2 rounded-full font-medium">VIEW ALL WORKS</button>
+        </div>
+    </div>
   );
 }
+
+export default Hero;
