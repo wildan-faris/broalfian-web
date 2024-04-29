ac12d1ecadb6fa2304cdbbe27d154da46fb79eab
Alfian Ramadani <Alfianr792@gmail.com>
2024-04-29 09:11:07 +0800

    feat : Add Projects Page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function Projects() {
+    return (
+        <section className="bg-[#161513] text-white ">
+            <div className="container mx-auto px-14">
+
+        <Navbar />
+        <div className="h-[70vh]">
+
+        </div>
+        <Footer />
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
