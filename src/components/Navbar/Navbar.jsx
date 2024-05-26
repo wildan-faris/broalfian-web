34cad88d0ca9f4bac68d334afa8f09593605ee54
Alfian Ramadani <Alfianr792@gmail.com>
2024-05-26 09:28:00 +0800

    feat : adding responsive mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,84 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import NavbarMobile from "./NavbarMobile";
+
 
 export default function Navbar() {
-  const navigate = useNavigate();
-  return (
 
-    <nav className=" py-4">
-      <div className="flex justify-between">
+
+
+  const navigate = useNavigate();
+  const [scrolling, setScrolling] = React.useState(false);
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  
+  const  handleScroll = () => {
+    if (window.scrollY > 40   ) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+
+    }
+  } 
+  return (
+    <nav
+      className={`fixed top-0  w-full py-4 px-7 md:px-7  z-50 rounded-b-xl   ${
+        scrolling ? "navbar-scroll" : "navbar-default"
+      }`}
+    >
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto">
         <div className="flex space-x-10 justify-center items-center">
           <div>
-
-            <a className="text-2xl md:text-3xl font-inter font-semibold" href="">
+            <a
+              className="text-2xl md:text-3xl font-inter font-semibold"
+              href=""
+            >
               PFOLIO.
             </a>
           </div>
-          <div className=" space-x-10 text-slate-300 hidden md:block text-base md:text-sm">
-            <Link to="/" className="nav-link" >
+          <div className="menu space-x-10 text-slate-300 hidden md:block text-base md:text-sm">
+            <HashLink
+              to="/"
+              className="nav-link"
+            >
               Home
-            </Link>
-            <Link to="/projects" className="nav-link" >
+            </HashLink>
+            <HashLink
+              smooth
+              to="/#projects"
+              className="nav-link"
+            >
               Projects
-            </Link>
-            <Link to="/about" className="nav-link" >
+            </HashLink>
+            <Link
+              to="/about"
+              className="nav-link"
+            >
               About
             </Link>
-            <Link to="/blog" className="nav-link" >
+            <Link
+              to="/blog"
+              className="nav-link"
+            >
               Blog
             </Link>
-
           </div>
         </div>
-        <div></div>
-        <div className="flex justify-center items-center">
+        <div className="action hidden md:flex justify-center items-center">
           <Link
-            className="text-xs md:text-base bg-white border-0 px-6 py-2 md:px-6 md:py-2 rounded-full text-black font-bold hover:bg-black hover:text-white border-b-[1px] text-center"
+            className="btn-action text-xs md:text-base bg-white border-0 px-6 py-2 md:px-6 md:py-2 rounded-full text-black font-bold hover:bg-black hover:text-white border-b-[1px] text-center"
             to="/contact"
           >
             LET'S TALK
           </Link>
         </div>
+        {/* Mobile */}
+        <NavbarMobile />
       </div>
     </nav>
   );
 }
-
