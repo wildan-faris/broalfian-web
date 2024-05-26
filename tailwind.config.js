b9a58361ffb0a40f86c54031e377f2cf70cdeafb
Alfian Ramadani <Alfianr792@gmail.com>
2024-05-26 09:39:17 +0800

    feat : install new config/plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,20 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
-
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
-    extend:{
+    extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
         caveat: ["Caveat", "sans-serif"],
         raleway: ["Raleway", "sans-serif"],
-      }
-    }
-   
- 
+        ubuntu: ["Ubuntu", "sans-serif"],
+        plusjakarta: ["Plus Jakarta Sans", "sans-serif"]
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin')
-
-  ],
-}
+  plugins: [require("flowbite/plugin"), require("daisyui"), require("@tailwindcss/typography")],
+};
