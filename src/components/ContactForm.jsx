fa6724f4297afee0660e3c03400f61d923da217f
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-19 07:10:51 +0800

    feat: Update ContactForm component styling and labels

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -25,29 +25,74 @@ function ContactForm(props) {
     
     }
     return (
-        <>
+      <>
         <form onSubmit={HandleSubmit}>
-        <div className="space-y-4">
-
+          <div className="space-y-4">
             <div className="flex flex-col gap-1">
-
-                <label htmlFor="NameInput" className="font-inter font-medium text-base text-primary"> Your Name</label>
-                <input id="NameInput" className="py-4 px-5 rounded-md bg-[#1C1C22] text-white border-0" type="text" placeholder="Enter your name" name="name" value={form.name} onChange={HandleChange} />
+              <label
+                htmlFor="NameInput"
+                className="font-inter font-medium text-base text-primary"
+              >
+                {' '}
+                Your Name
+              </label>
+              <input
+                id="NameInput"
+                className="py-4 px-5 rounded-md bg-background1 text-primary border-1"
+                type="text"
+                placeholder="Enter your name"
+                name="name"
+                value={form.name}
+                onChange={HandleChange}
+              />
             </div>
             <div className="flex flex-col gap-1">
-
-                <label htmlFor="EmailInput" className="font-inter font-medium text-base text-primary"> Your E-mail</label>
-                <input id="EmailInput" name="email" className="py-4 px-5 rounded-md bg-[#1C1C22] text-white border-0" type="text" placeholder="Enter your e-mail" value={form.email} onChange={HandleChange} />
+              <label
+                htmlFor="EmailInput"
+                className="font-inter font-medium text-base text-primary"
+              >
+                {' '}
+                Your E-mail
+              </label>
+              <input
+                id="EmailInput"
+                name="email"
+                className="py-4 px-5 rounded-md bg-background1 text-primary border-1"
+                type="text"
+                placeholder="Enter your e-mail"
+                value={form.email}
+                onChange={HandleChange}
+              />
             </div>
             <div className="flex flex-col gap-1">
-
-                <label htmlFor="NameInput" className="font-inter font-medium text-base text-primary" > Tell me a bit more what you are looking for?</label>
-                <textarea id="NameInput" className="py-4 px-5 rounded-md bg-[#1C1C22] text-white border-[1px]" type="text" rows="4" cols="50" placeholder="" name="description" value={form.description} onChange={HandleChange}  ></textarea>
+              <label
+                htmlFor="NameInput"
+                className="font-inter font-medium text-base text-primary"
+              >
+                {' '}
+                Tell me a bit more what you are looking for?
+              </label>
+              <textarea
+                id="NameInput"
+                className="py-4 px-5 rounded-md bg-background1 text-primary border-1"
+                type="text"
+                rows="4"
+                cols="50"
+                placeholder=""
+                name="description"
+                value={form.description}
+                onChange={HandleChange}
+              ></textarea>
             </div>
-        <button type="submit" className="bg-gradient-to-r from-[#B16CEA] to-[#FF7B5D] rounded-full md:px-7 text-xl font-medium md:py-3 px-4 py-1">SUBMIT NOW</button>
-        </div>
-    </form>
-        </>
+            <button
+              type="submit"
+              className="bg-gradient-to-r from-[#B16CEA] to-[#FF7B5D] rounded-full md:px-7 text-xl font-medium md:py-3 px-4 py-1"
+            >
+              SUBMIT NOW
+            </button>
+          </div>
+        </form>
+      </>
     );
 }
 
