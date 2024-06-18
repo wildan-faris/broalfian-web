5c39e6dabed32ea0653d425eed5f14d0fd9b71df
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-19 07:12:42 +0800

    feat: Add SVG icons for Visual Studio Code, Figma, Tailwind, GitHub, Bootstrap, and Vite

---
+++ src/assets/stack/icon.jsx
@@ -0,0 +1,44 @@
+import bootstrap from './bootstrap.svg';
+import canva from './canva.svg';
+import excel from './excel.svg';
+import figma from './figma.svg';
+import git from './git.svg';
+import github from './github.svg';
+import html from './html.svg';
+import laravel from './laravel.svg';
+import ngrok from './ngrok.svg';
+import mysql from './mysql.svg';
+import nodeJs from './node-js.svg';
+import php from './php.svg';
+import postman from './postman.svg';
+import reactLogo from './react.svg';
+import tailwindCss from './tailwind.svg';
+import vscode from './visual-studio-code.svg';
+import vite from './vite.svg';
+
+// Bagian pertama dari ikon
+const Icons1 = [
+  { src: canva, desc: 'Graphic Design', alt: 'Canva' },
+  { src: bootstrap, desc: 'Front-End Development', alt: 'Bootstrap' },
+  { src: excel, desc: 'Data Analysis', alt: 'Excel' },
+  { src: figma, desc: 'UI/UX Design', alt: 'Figma' },
+  { src: git, desc: 'Version Control', alt: 'Git' },
+  { src: github, desc: 'Version Control', alt: 'GitHub' },
+  { src: html, desc: 'Basic Development', alt: 'HTML' },
+  { src: laravel, desc: 'Back-End Development', alt: 'Laravel' },
+];
+
+// Bagian kedua dari ikon
+const Icons2 = [
+  { src: ngrok, desc: 'Local Tunneling', alt: 'ngrok' },
+  { src: mysql, desc: 'Database Development', alt: 'MySQL' },
+  { src: nodeJs, desc: 'Back-End Development', alt: 'Node.js' },
+  { src: php, desc: 'Back-End Development', alt: 'PHP' },
+  { src: postman, desc: 'API Testing', alt: 'Postman' },
+  { src: reactLogo, desc: 'Front-End Development', alt: 'React' },
+  { src: tailwindCss, desc: 'Front-End Development', alt: 'Tailwind CSS' },
+  { src: vscode, desc: 'Code Editor', alt: 'Visual Studio Code' },
+  { src: vite, desc: 'Front-End Development', alt: 'Vite' },
+];
+
+export { Icons1, Icons2 };
