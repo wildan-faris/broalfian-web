bf503a20f802a4cd0ee66d1ad355164bba1741a0
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-19 07:08:55 +0800

    refactor: Update Tailwind configuration to include new colors, animation, and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
-    "node_modules/daisyui/dist/**/*.js",
-    "node_modules/react-daisyui/dist/**/*.js",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
     extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
+      colors: {
+        background1: 'var(--color-background1)',
+        background2: 'var(--color-background2)',
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+      },
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        caveat: ["Caveat", "sans-serif"],
-        raleway: ["Raleway", "sans-serif"],
-        ubuntu: ["Ubuntu", "sans-serif"],
-        plusjakarta: ["Plus Jakarta Sans", "sans-serif"]
+        inter: ['Inter', 'sans-serif'],
+        caveat: ['Caveat', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        plusjakarta: ['Plus Jakarta Sans', 'sans-serif'],
       },
     },
   },
-  plugins: [require("flowbite/plugin"), require("daisyui"), require("@tailwindcss/typography")],
+  plugins: [
+    require('flowbite/plugin'),
+    require('daisyui'),
+    require('@tailwindcss/typography'),
+  ],
 };
